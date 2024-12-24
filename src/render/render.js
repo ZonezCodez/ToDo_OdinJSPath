9ee906fbf07ff5ae2ef7abcdd74396eb3d921288
Zone_Codez <odincodezone@gmail.com>
2024-12-24 09:03:57 -0600

    made the render function needs an html object like a created p or div

---
+++ src/render/render.js
@@ -0,0 +1,14 @@
+const renderArea = document.getElementById('content'); // Expects the template to have a content area where you change the view
+
+export default function render(data,reset){
+    if(reset === true){
+        resetPage(); // Calls the resetpage function if reset variable is true;
+    }else{
+        renderArea.appendChild(data); // data is expected to be a html object like a div with the appendeded data inside can be a p with text up to a div full of stuff
+        return;
+    }
+}
+
+function resetPage(){
+    renderArea.textContent = ''; // This resets the render area before we append more data
+}
